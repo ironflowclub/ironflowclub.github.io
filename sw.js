72c79558016334e06135f4c0b4b27e5d0b4f6da5
Syed <syed.mujeebprojects@gmail.com>
2026-06-12 17:00:00 +0400

    clear chache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Bump CACHE_VERSION whenever you deploy updates
  */
 
-const CACHE_VERSION = 'v114';
+const CACHE_VERSION = 'v115';
 const CACHE_NAME = `ironflow-${CACHE_VERSION}`;
 
 const STATIC_ASSETS = [
