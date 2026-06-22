91563fc86d264993896110afb55136bffb50a1d5
Syed <syed.mujeebprojects@gmail.com>
2026-06-22 13:04:53 +0400

    Analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,13 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-S8T2LZFCZW';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-S8T2LZFCZW');
+})();
+
